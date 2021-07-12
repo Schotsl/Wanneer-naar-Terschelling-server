87d5b05ccc5da3946a3689dad4f8253542bb9523
Schotsl <sjorsvanholst@gmail.com>
2021-07-12 21:29:21 +0200

    🐛 Debugging deploy

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { Application } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 import { bodyValidation, errorHandler } from "./middleware.ts";
 
-// import router from "./router.ts";
+import router from "./router.ts";
 
 const application = new Application();
 
@@ -10,8 +10,8 @@ application.use(errorHandler);
 application.use(bodyValidation);
 
 application.use(oakCors());
-// application.use(router.routes());
-// application.use(router.allowedMethods());
+application.use(router.routes());
+application.use(router.allowedMethods());
 
 // application.listen({ port: 8080 });
 
